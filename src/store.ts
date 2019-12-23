762325652404b106fce40bc9a87c58cbfedb3873
sijeong <sijeong@outlook.com>
2019-12-23 11:01:00 +0900

    chang EduHistory using mobx store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,35 @@
+import { create } from "domain";
+
+export type Education = {
+    name: string;
+    startDate: Date;
+    endDate: Date;
+    result: string;
+}
+
+const Educations: Education[] = [
+    {
+        name: 'Service',
+        startDate: new Date(2019, 11, 7, 10, 0),
+        endDate: new Date(2019, 11, 7, 12, 0),
+        result: 'passed'
+    },
+    {
+        name: 'Basic Cooking',
+        startDate: new Date(2019, 11, 9, 10, 0),
+        endDate: new Date(2019, 11, 9, 12, 0),
+        result: 'passed'
+    }
+];
+
 export const createStore = () => {
     const store = {
-        
-    }
+        get allEducations() {
+            return Educations;
+        }
+    };
+
+    return store;
 }
+
+export type TStore = ReturnType<typeof createStore>;
